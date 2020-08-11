f66b646b97f5f8aff3ff0804c68761e766aeecb2
Michael Grandison <african.genetic.survival@gmail.com>
2020-08-10 20:19:03 -0500

    pm2 ecosystem test. incl react build folder

---
+++ frontend/build/precache-manifest.38aef616b80a77b147e5f0325912325b.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7ace9d5128ddd8e7ff7b0f43405e832d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "684e7cc289cac14cf55e",
+    "url": "/static/css/2.b27a6e0d.chunk.css"
+  },
+  {
+    "revision": "1e7642240ebbbee31d86",
+    "url": "/static/css/main.473c2963.chunk.css"
+  },
+  {
+    "revision": "684e7cc289cac14cf55e",
+    "url": "/static/js/2.dae8398b.chunk.js"
+  },
+  {
+    "revision": "f9aeaf38ec104f2733ca74e8c101073e",
+    "url": "/static/js/2.dae8398b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1e7642240ebbbee31d86",
+    "url": "/static/js/main.a6af4ff3.chunk.js"
+  },
+  {
+    "revision": "e79b6053c5ba3afe8bfe",
+    "url": "/static/js/runtime-main.a10d6ed4.js"
+  },
+  {
+    "revision": "8631702de01e07a5c200ebf294df1e2f",
+    "url": "/static/media/loading.8631702d.gif"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
