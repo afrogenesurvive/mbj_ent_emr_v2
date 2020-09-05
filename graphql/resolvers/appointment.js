2ba984389a39ff096298722f5d99ce1de8a5f98b
Michael Grandison <african.genetic.survival@gmail.com>
2020-09-04 20:24:52 -0500

    time zone check

--- graphql/resolvers/appointment.js
+++ graphql/resolvers/appointment.js
@@ -594,6 +594,7 @@ module.exports = {
       const patient = await Patient.findById({_id: args.patientId})
       const today = moment();
 
+      console.log('4:',moment());
       console.log('1:',moment().format('YYYY-MM-DD'));
       console.log('2:',moment(args.appointmentInput.date).format('YYYY-MM-DD'));
       console.log('3:',moment.unix(args.appointmentInput.date.substr(0,10)).add(1,'days').format('YYYY-MM-DD'));
